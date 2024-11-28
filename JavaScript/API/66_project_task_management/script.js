9a3503869ae5acd94c3265e9af95ab1885c917f5
Mahavir745 <mahavir777kumar@gmail.com>
2024-11-28 09:21:50 +0530

    update

--- JavaScript/API/66_project_task_management/script.js
+++ JavaScript/API/66_project_task_management/script.js
@@ -1,224 +1,216 @@
+const addTask = document.querySelector("#addTask");
+const pendingContainer = document.querySelector("#pending_child1_items2");
+const completeContainer = document.querySelector("#completed_task");
 
-const addTask = document.querySelector("#addTask")
-const pending_child1_items2 = document.querySelector("#pending_child1_items2")
-const completed_task = document.querySelector("#completed_task")
+let allDetailsTask = [];
 
-let allDetailsTask = []
-let completedTask = []
+addTask.addEventListener("click", (e) => {
+  e.preventDefault();
 
-addTask.addEventListener("click",(e)=>{
-  e.preventDefault()
+  const taskTitle = document.querySelector("#taskTitle");
+  const dueDate = document.querySelector("#dueDate");
+  const taskDetails = document.querySelector("#taskDetails");
+  const priority = document.querySelector("#priority");
 
-    let div = document.createElement("div")
-    div.classList.add("pendingChild")
+  let arrangeDate = dueDate.value.split("-").reverse().join("-");
 
-    let p1 = document.createElement("p")
-    let p2 = document.createElement("p")
-    let p3 = document.createElement("p")
-    let p4 = document.createElement("p")
+  let idname = Date.now();
+  let task = {
+    title: taskTitle.value,
+    date: arrangeDate,
+    details: taskDetails.value,
+    priority: priority.value,
+    id: idname,
+    status: "pending"
+  };
 
-    p1.classList.add("p1")
-    p2.classList.add("p2")
-    p3.classList.add("p3")
-    p4.classList.add("p4")
+  taskTitle.value = ""
+  dueDate.value = ""
+  taskDetails.value = ""
+  priority.value = ""
+  
+  allDetailsTask.push(task);
+  TaskAdded(task); 
+});
 
-    let element1 = document.createElement("div")
-    element1.classList.add("element1")
+const TaskAdded = (task) => {
+  let taskContainer = document.createElement("div");
+  taskContainer.classList.add("pendingChild");
+  taskContainer.setAttribute("id", `${task.id}`);
 
-    let element2 = document.createElement("div")
-    element2.classList.add("element2")
+  let p1 = document.createElement("p");
+  let p2 = document.createElement("p");
+  let p3 = document.createElement("p");
+  let p4 = document.createElement("p");
 
-    let label = document.createElement("label")
-    label.textContent = "Marks as completed"
+  p1.classList.add("p1");
+  p2.classList.add("p2");
+  p3.classList.add("p3");
+  p4.classList.add("p4");
 
-    let btn_done = document.createElement("button")
-    btn_done.setAttribute("class","taskcompleted")
+  let element1 = document.createElement("div");
+  element1.classList.add("element1");
 
-    let delete_btn = document.createElement("button")
-    delete_btn.setAttribute("class","delete_btn")
-    delete_btn.innerHTML = "Delete"
+  let element2 = document.createElement("div");
+  element2.classList.add("element2");
 
-    div.append(p1,p2,element1,element2)
-    element1.append(p3,p4)
-    element2.append(label,btn_done,delete_btn)
+  let label = document.createElement("label");
+  label.textContent = "Mark as completed";
 
-    const taskTitle = document.querySelector("#taskTitle").value
-    const dueDate = document.querySelector("#dueDate").value
-    const taskDetails = document.querySelector("#taskDetails").value
-    const priority = document.querySelector("#priority").value
-    let arrangeDate = dueDate.split("-").reverse().join("-")
+  let btn_done = document.createElement("button");
+  btn_done.setAttribute("class", "taskcompleted");
+  btn_done.setAttribute("id", `${task.id}`);
 
-    let task = {
-      title: taskTitle,
-      date: arrangeDate,
-      details: taskDetails,
-      priority: priority
-    }
+  let delete_btn = document.createElement("button");
+  delete_btn.setAttribute("class", "delete_btn");
+  delete_btn.setAttribute("id", `${task.id}`);
+  delete_btn.innerHTML = "Delete";
 
-    p1.textContent = task.title
-    p2.textContent = task.details
-    p3.textContent = task.date
-    p4.textContent = task.priority
+  taskContainer.append(p1, p2, element1, element2);
+  element1.append(p3, p4);
+  element2.append(label, btn_done, delete_btn);
 
-    let count = 0
+  p1.textContent = task.title;
+  p2.textContent = task.details;
+  p3.textContent = task.date;
+  p4.textContent = task.priority;
 
-    if(taskTitle != ""){
-      count+=1
-    }else{
-      alert("Task title")
-    }
-
-    if(dueDate != ""){
-      count++
-    }else{
-      alert("Enter your task date")
-    }
-
-    if(taskDetails != ""){
-      count++
-    }else{
-      alert("Enter your task")
-    }
-
-    if(priority != "none"){
-      count++
-    }
-    else{
-      alert("choose your priority")
-    }
-
-    if(count === 4){
-      allDetailsTask.push(div)
-      pending_child1_items2.appendChild(div)
-    }
-
-
-  delete_btn.addEventListener("click",()=>{
-    div.remove()
-  })
-
-  btn_done.addEventListener("click",()=>{
-
-    completedTask.push(div)
-
-    completedTask.forEach((ele)=>{
-      completed_task.append(ele)
-      btn_done.style.background = 'yellowgreen'
-    })
-  })
-
-})
-
-
-
-const filter1_submit = document.querySelector("#filter1_submit")
-
-filter1_submit.addEventListener("click",()=>{
-  const filter1_date = document.querySelector("#filter1_date").value
-  const filter1_medium = document.querySelector("#filter1_medium").value
-
-  let arrangeDate = filter1_date.split("-").reverse().join("-")
-
-  if(filter1_date != "" && filter1_medium != "none"){
-    completedTask.map((ele)=>{
-      let date = ele.children[2].children[0].innerText
-      let medium = ele.children[2].children[1].innerText
-
-      if(date === arrangeDate && medium === filter1_medium){
-        ele.style.display = "flex"
-      }
-      else{
-        ele.style.display = "none"
-      }
-    })
+  if (task.status === "pending") {
+    pendingContainer.append(taskContainer);
+  } else {
+    completeContainer.append(taskContainer);
   }
-  else if(filter1_date != ""){
-    completedTask.map((ele)=>{
-      let date = ele.children[2].children[0].innerText
-      if(date !== arrangeDate){
-        ele.style.display = "none"
+
+//todo: complete Btn
+
+  btn_done.addEventListener("click", (e) => {
+    allDetailsTask.forEach((ele) => {
+      let taskId = Number(ele.id);
+      let btnId = Number(e.target.id);
+      if (taskId === btnId) {
+        ele.status = "completed";
+        completeTasks(ele);
       }
-      else{
-        ele.style.display = "flex"
-      }
-    })
+    });
+  });
+
+//todo: delete Btn
+  delete_btn.addEventListener("click", (e) => {
+    allDetailsTask = allDetailsTask.filter((ele) => ele.id !== task.id);
+    taskContainer.remove();
+  });
+};
+
+const completeTasks = (task) => {
+  const taskContainer = document.getElementById(task.id);
+  if (taskContainer) {
+    taskContainer.remove();
   }
-  else if(filter1_medium != "none"){
-    completedTask.map((ele)=>{
-      let medium = ele.children[2].children[1].innerText
+  TaskAdded(task); 
+};
+
+
+// const filter1_submit = document.querySelector("#filter1_submit")
+
+// filter1_submit.addEventListener("click",()=>{
+//   const filter1_date = document.querySelector("#filter1_date").value
+//   const filter1_medium = document.querySelector("#filter1_medium").value
+
+//   let arrangeDate = filter1_date.split("-").reverse().join("-")
+
+//   if(filter1_date != "" && filter1_medium != "none"){
+//     completedTask.map((ele)=>{
+//       let date = ele.children[2].children[0].innerText
+//       let medium = ele.children[2].children[1].innerText
+
+//       if(date === arrangeDate && medium === filter1_medium){
+//         ele.style.display = "flex"
+//       }
+//       else{
+//         ele.style.display = "none"
+//       }
+//     })
+//   }
+//   else if(filter1_date != ""){
+//     completedTask.map((ele)=>{
+//       let date = ele.children[2].children[0].innerText
+//       if(date !== arrangeDate){
+//         ele.style.display = "none"
+//       }
+//       else{
+//         ele.style.display = "flex"
+//       }
+//     })
+//   }
+//   else if(filter1_medium != "none"){
+//     completedTask.map((ele)=>{
+//       let medium = ele.children[2].children[1].innerText
  
-      if(filter1_medium !== medium){
-       ele.style.display = "none"
-      }
-      else{
-       ele.style.display = "flex"
-      }
+//       if(filter1_medium !== medium){
+//        ele.style.display = "none"
+//       }
+//       else{
+//        ele.style.display = "flex"
+//       }
    
-     })
-  }
-  else{
-    completedTask.map((ele)=>{
-      ele.style.display = "flex"
-    })
-  }
-})
+//      })
+//   }
+//   else{
+//     completedTask.map((ele)=>{
+//       ele.style.display = "flex"
+//     })
+//   }
+// })
 
 
+// const filter2_submit = document.querySelector("#filter2_submit")
 
+// filter2_submit.addEventListener("click",()=>{
+//   const filter2_date = document.querySelector("#filter2_date").value
+//   const filter2_medium = document.querySelector("#filter2_medium").value
+//   let arrangeDate = filter2_date.split("-").reverse().join("-")
 
+//   if(filter2_date != "" && filter2_medium != "none"){
+//     allDetailsTask.map((ele)=>{
+//       let date = ele.children[2].children[0].innerText
+//       let medium = ele.children[2].children[1].innerText
 
+//       if(date === arrangeDate && medium === filter2_medium){
+//         ele.style.display = "flex"
+//       }
+//       else{
+//         ele.style.display = "none"
+//       }
+//     })
+//   }
+//   else if(filter2_date != ""){
+//     allDetailsTask.map((ele)=>{
+//       let date = ele.children[2].children[0].innerText
 
-const filter2_submit = document.querySelector("#filter2_submit")
+//       if(date !== arrangeDate){
+//         ele.style.display = "none"
+//       }
+//       else{
+//         ele.style.display = "flex"
+//       }
+//     })
+//   }
+//   else if(filter2_medium != "none"){
+//     allDetailsTask.map((ele)=>{
+//       let medium = ele.children[2].children[1].innerText
 
-filter2_submit.addEventListener("click",()=>{
-  const filter2_date = document.querySelector("#filter2_date").value
-  const filter2_medium = document.querySelector("#filter2_medium").value
-  let arrangeDate = filter2_date.split("-").reverse().join("-")
-
-  if(filter2_date != "" && filter2_medium != "none"){
-    allDetailsTask.map((ele)=>{
-      let date = ele.children[2].children[0].innerText
-      let medium = ele.children[2].children[1].innerText
-
-      if(date === arrangeDate && medium === filter2_medium){
-        ele.style.display = "flex"
-      }
-      else{
-        ele.style.display = "none"
-      }
-    })
-  }
-  else if(filter2_date != ""){
-    allDetailsTask.map((ele)=>{
-      let date = ele.children[2].children[0].innerText
-
-      if(date !== arrangeDate){
-        ele.style.display = "none"
-      }
-      else{
-        ele.style.display = "flex"
-      }
-    })
-  }
-  else if(filter2_medium != "none"){
-    allDetailsTask.map((ele)=>{
-      let medium = ele.children[2].children[1].innerText
-
-      if(filter2_medium !== medium){
-       ele.style.display = "none"
-      }
-      else{
-       ele.style.display = "flex"
-      }
+//       if(filter2_medium !== medium){
+//        ele.style.display = "none"
+//       }
+//       else{
+//        ele.style.display = "flex"
+//       }
    
-     })
-  }
-  else{
-    allDetailsTask.map((ele)=>{
-      ele.style.display = "flex"
-    })
-  }
-})
-
-
-
+//      })
+//   }
+//   else{
+//     allDetailsTask.map((ele)=>{
+//       ele.style.display = "flex"
+//     })
+//   }
+// })
--- JavaScript/DOM/61_challenges_dom/script.js
+++ JavaScript/DOM/61_challenges_dom/script.js
@@ -1,39 +1,31 @@
+let submit_btn = document.querySelector("#submit_btn");
 
-let submit_btn = document.querySelector("#submit_btn")
+submit_btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Form submitted");
+});
 
-submit_btn.addEventListener("click",(e)=>{
-  e.preventDefault()
-  console.log("Form submitted")
-})
+let toggleDivs = document.querySelectorAll(".toggleDiv");
 
+toggleDivs.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    ele.classList.toggle("bg_change");
+  });
+});
 
-let allDiv = document.querySelectorAll("div")
+let colorDiv = document.querySelector("#colorChanger");
+let separateColorDiv = document.querySelector("#separateColorDiv");
 
-allDiv.forEach((ele)=>{
-  ele.addEventListener("click",()=>{
-    ele.classList.toggle("bg_change")
-  })
-})
-
-
-
-let colorDiv = document.querySelector("#colorChanger")
-
-let color = colorDiv.childNodes
-color.forEach((ele)=>{
-  ele.addEventListener("click",()=>{
-    if(ele.textContent == 'Red'){
-      colorDiv.style.backgroundColor = "red"
+colorDiv.childNodes.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    if (ele.textContent == 'Red') {
+      separateColorDiv.style.backgroundColor = "red";
+    } else if (ele.textContent == "Yellow") {
+      separateColorDiv.style.backgroundColor = "yellow";
+    } else if (ele.textContent == "Blue") {
+      separateColorDiv.style.backgroundColor = "blue";
+    } else if (ele.textContent == "Green") {
+      separateColorDiv.style.backgroundColor = "green";
     }
-    else if(ele.textContent=="Yellow"){
-      colorDiv.style.backgroundColor = "yellow"
-    }
-    else if(ele.textContent=="Blue"){
-      colorDiv.style.backgroundColor = "blue"
-    }
-    else if(ele.textContent=="Green"){
-      colorDiv.style.backgroundColor = "green"
-    }
-    
-  })
-})
+  });
+});
